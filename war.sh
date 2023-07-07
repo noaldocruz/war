2285e6d5dc6aabe6ffaa2b171690ae9bedd39dcd
noaldocruz <63937237+noaldocruz@users.noreply.github.com>
2023-07-07 00:33:21 -0500

    Update war.sh

--- war.sh
+++ war.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update && sudo apt install docker docker-compose && sudo apt install mailutils  && sudo apt-get install ssmtp && sudo apt  install mutt && sudo apt install unzip && sudo apt install zip -y 
 wget https://raw.githubusercontent.com/noaldocruz/war/main/ssmtp.conf
-sed -i "s/SERVER_URL_REPLACE/$2/" ssmtp.conf
+sed -i "s/MY_FAVORITE_PASS/$2/" ssmtp.conf
 sudo mv ssmtp.conf /etc/ssmtp/ssmtp.conf
 SERVER_URL=$(curl ifconfig.me)
 wget https://raw.githubusercontent.com/noaldocruz/war/main/wireguard.yaml
