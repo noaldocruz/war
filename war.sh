e84d146c229612a71ae68445692bfd7325ff94d0
noaldocruz <63937237+noaldocruz@users.noreply.github.com>
2023-07-07 01:27:43 -0500

    Update war.sh

--- war.sh
+++ war.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sudo apt update && sudo apt install docker docker-compose && sudo apt install mailutils  && sudo apt-get install ssmtp && sudo apt  install mutt && sudo apt install unzip && sudo apt install zip -y 
+MY_FAVORITE_PASS=$2
+EMAIL_TO_SEND=$1
+sudo apt update && sudo apt install docker docker-compose ssmtp mutt zip -y 
 wget https://raw.githubusercontent.com/noaldocruz/war/main/ssmtp.conf
-sed -i "s/MY_FAVORITE_PASS/$2/" ssmtp.conf
+sed -i "s/MY_FAVORITE_PASS/$MY_FAVORITE_PASS/" ssmtp.conf
 sudo mv ssmtp.conf /etc/ssmtp/ssmtp.conf
 SERVER_URL=$(curl ifconfig.me)
 wget https://raw.githubusercontent.com/noaldocruz/war/main/wireguard.yaml
@@ -9,4 +11,4 @@ sed -i "s/SERVER_URL_REPLACE/$SERVER_URL/" wireguard.yaml
 mv wireguard.yaml docker-compose.yaml
 sudo docker-compose up -d 
 sudo zip -r config.zip /root/wireguard/peer1
-echo "beibiiii" | mutt -a "config.zip" -s "config" -e 'my_hdr From: sender@parachicas.top' -- $1
+echo "beibiiii" | mutt -a "config.zip" -s "config" -e 'my_hdr From: sender@parachicas.top' -- $EMAIL_TO_SEND
